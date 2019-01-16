256d25603904cbaf345074264890a404aac30013
shubhampalwar <shubham.palwar@successive.tech>
2019-01-16 13:43:56 +0530

    feat:create two pattern functions Diamond and EquilateralTriangle

---
+++ Extra/Patterns/Diamond.js
@@ -0,0 +1,25 @@
+function Diamond(n) {
+  console.log('Printing diamond of '+n+' rows....');
+  let pattern = '';
+  for(let i = 1; i <= n; i++) {
+    for(let j = i; j <= (n-1); j++) {
+      pattern += ' ';
+    }
+    for(let k = 1; k <= i; k++) {
+      pattern += "* ";
+    }
+    console.log(pattern);
+    pattern = '';
+  }
+  for(let i = n; i >= 1; i--) {
+    for(let j = i; j <= (n-1); j++) {
+      pattern += ' ';
+    }
+    for(let k = 1; k <= i; k++) {
+      pattern += "* ";
+    }
+    console.log(pattern);
+    pattern = '';
+  }
+}
+Diamond(5);
