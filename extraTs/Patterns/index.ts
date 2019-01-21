9bf27f6c3d894ccc6b1b93c05178c35b793036ab
harshwardhan0812 <46667194+harshwardhan0812@users.noreply.github.com>
2019-01-21 18:31:54 +0530

    Update index.ts

--- extraTs/Patterns/index.ts
+++ extraTs/Patterns/index.ts
@@ -1,8 +1,8 @@
-console.log("Inside Pattern Index");
+// console.log("Inside Pattern Index");
 //import diamondPattern  from './Diamond';
 //import trianglePattern from './EquilateralTriangle';
 //export {diamondPattern};
 //export {trianglePattern};
 export { default as diamondPattern } from "./Diamond";
 export { default as trianglePattern } from "./EquilateralTriangle";
-console.log("outside Pattern Index");
+// console.log("outside Pattern Index");
